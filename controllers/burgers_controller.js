e00489ecc375529f9e59fceadda96d6d9bc32911
Dave Hawkins <dhawkins01@gmail.com>
2019-07-09 16:07:04 -0400

    home slice route added

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,5 +2,24 @@ var express = require("express");
 
 var router = express.Router();
 
+
+
+// Import the model burger
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    
+      res.render("index");
+    });
+  
+
+
+
+
+
+
+
+
 // Export routes for server.js to use.
 module.exports = router;
